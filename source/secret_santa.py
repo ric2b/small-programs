b583c67c4f8390262d0ff8585769cca67453add3
ric2b <ricardo.amendoeira@ist.utl.pt>
2014-11-03 22:06:26 +0000

    already matches the gifter with the giftee

--- source/secret_santa.py
+++ source/secret_santa.py
@@ -3,8 +3,10 @@ import pprint
 import random
 
 user_input = raw_input("Please input the names with a space in between >> ").split(" ")
+
 n_people = len(user_input)
 names = zip(range(n_people), user_input)
+
 print names
 
 raffle = []
@@ -14,3 +16,9 @@ for key, name in names:
 	
 print raffle
 
+gifts_to = []
+
+for gifter_name, giftee_key in raffle:
+	gifts_to.append((gifter_name, names[giftee_key][1]))
+	
+print gifts_to
