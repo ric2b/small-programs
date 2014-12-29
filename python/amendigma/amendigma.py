9e0109652b6728cc2c00822de6bc07fecd6c88f0
Ricardo Amendoeira <ricardo.amendoeira@ist.utl.pt>
2014-12-29 18:24:14 +0000

    Plugboard halfway implemented

--- python/amendigma/amendigma.py
+++ python/amendigma/amendigma.py
@@ -1,4 +1,5 @@
 import sys
+import string
 import time
 
 def check_args():
@@ -7,11 +8,11 @@ def check_args():
 		print "Usage: python " + sys.argv[0] + " <message_file> (mandatory) [settings_file] (optional)\n"
 		exit()
 	if len(sys.argv) == 2:
-		return sys.argv[1], None
+		return sys.argv[1], "default.txt"
 	else:
-		return sys.argv[1], sys, argv[2]
+		return sys.argv[1], sys.argv[2]
 
-def transcodeMessage(filename):
+def readMessage(filename):
 	
 	try:
 		f = open(filename, 'r')
@@ -19,17 +20,49 @@ def transcodeMessage(filename):
 		print "Failed to open '" + filename + "'."
 		exit()
 	
-	while True:
-		character = f.read(1)
-		time.sleep (0.2)
-		if character == '':
-			break
-		print character 
+	message = f.read()
 
 	f.close()	# close the file
-	return None
+	return message
+
+def getplugboard(plugboardFile):
+	plugboard = {}
+
+	try:
+		f = open(plugboardFile, 'r')
+	except IOError:
+		print "Failed to open '" + plugboardFile + "'."
+		print "Will run with unconnected plugboard."
+
+	for digit in string.digits:
+		plugboard[digit] = digit
+
+#	for line in f:
+#		plugboard	
+
+	return {'a': 'b', 'b': 'c', 'c': 'd'}
+
+def rotor1(character):
+	plugboard = getplugboard("plugboardA.txt")
+
+	try:
+		transcoded = plugboard[character]
+	except KeyError:
+		transcoded = character
+
+	return transcoded
+
+
+def transcodeMessage(message):
+	transcodedMessage = []
+
+	for i in range(0, len(message)):
+		transcodedMessage += rotor1(message[i])
+
+	return ''.join(transcodedMessage)
 
 
 messageFile, settingsFile = check_args()
-#setup()
-message = transcodeMessage(messageFile)
+message = readMessage(messageFile)
+transcodedMessage = transcodeMessage(message)
+print transcodedMessage
